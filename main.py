c292c6310ec6f0afd924f452a836d6bceb258638
Huguitis <mail@huguitisnodes.host>
2023-11-21 18:58:52 +0100

    Updates

--- main.py
+++ main.py
@@ -1,50 +1,43 @@
-from http.client import HTTPSConnection 
-from sys import stderr 
-from json import dumps 
-from time import sleep 
+import http.client
 import json
-#Load Config
+import time
+
 with open('./config.json') as f:
-  data = json.load(f)
-  for c in data['Config']:
-        print('Loading...')
-channelid = c['channelid'] #modify this in config.json
-token = c['token'] #modify this in config.json
-message = c['message'] #modify this in config.json
+    config_data = json.load(f)
+    channel_id = config_data['Config'][0]['channelid']  # config.json
+    token = config_data['Config'][0]['token']  # config.json
+    message = config_data['Config'][0]['message']  # config.json
+
 header_data = { 
-	"content-type": "application/json", 
-	"user-agent": "discordapp.com", 
-	"authorization": token
+    "Content-Type": "application/json", 
+    "User-Agent": "DiscordBot", 
+    "Authorization": token  
 } 
- 
+
 def get_connection(): 
-	return HTTPSConnection("discordapp.com", 443) 
- 
+    return http.client.HTTPSConnection("discord.com", 443) 
+
 def send_message(conn, channel_id, message_data): 
     try: 
-        conn.request("POST", f"/api/v7/channels/{channel_id}/messages", message_data, header_data) 
+        conn.request("POST", f"/api/v10/channels/{channel_id}/messages", message_data, header_data) 
         resp = conn.getresponse() 
          
         if 199 < resp.status < 300: 
-            print("Message Sent") 
-            pass 
- 
+            print("Message Sent.") 
         else: 
-            stderr.write(f"HTTP {resp.status}: {resp.reason}\n") 
-            pass 
- 
+            print(f"HTTP {resp.status}: {resp.reason}")
     except: 
-        stderr.write("Error\n") 
- 
+        print("Error.") 
+
 def main(): 
-	message_data = { 
-		"content": message, 
-		"tts": "false"
-	} 
- 
-	send_message(get_connection(), channelid, dumps(message_data)) 
- 
+    message_data = { 
+        "content": message, 
+        "tts": False
+    } 
+
+    send_message(get_connection(), channel_id, json.dumps(message_data)) 
+
 if __name__ == '__main__': 
-	while True:    
-		main()      
-		sleep(3600) #How often the message will be sent (in seconds), every 1 hour = 3600
+    while True:    
+        main()      
+        time.sleep(3600)  # 1 Hour
